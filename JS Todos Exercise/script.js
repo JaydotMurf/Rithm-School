156b38f14bbaf7191b43fbf5eddd1fd42011768a
jor-el <jerraill.d.murphy@gmail.com>
2023-03-15 23:51:39 +0100

    MOdified localStorage but can't get retrive task after refresh

--- JS Todos Exercise/script.js
+++ JS Todos Exercise/script.js
@@ -1,37 +1,30 @@
 // global selectors
 
-// this is the button beside the li
 const completeButtons = document.querySelectorAll("li button");
-// this is the form
 const form = document.querySelector("#add-task");
-// this is the input bar
 const input = document.querySelector("#taskSubmission");
-// this is the ul
 const toDoList = document.querySelector("#todoList");
 
 form.addEventListener("submit", function (e) {
   // Ensure the submission of the form doesn't refresh the page
   e.preventDefault();
 
+  let taskValue = input.value.trim();
+
+  //   console.log(taskValue);
+
   // console.log(input.value);
 
-  // create new tasks/button pair
-  const newTask = document.createElement("li");
-  const newCompleteBtn = document.createElement("button");
-
-  // update task text, connect the button to the li, add li to list
-  newCompleteBtn.innerText = "Completed ";
-  newTask.innerText = input.value.trim() + " ";
-
-  newTask.appendChild(newCompleteBtn);
-  toDoList.appendChild(newTask);
+  // create new tasks/button pair and append to the list
+  createAndAppendTask(input.value.trim());
 
   // Save the task to local storage
-  if (newTask.innerText !== "") {
-    const tasks = JSON.parse(localStorage.getItem("tasks"));
-    localStorage.setItem("tasks", JSON.stringify(input.value));
+  if (input.value.trim !== "") {
+    // stringify tasks
+    localStorage.setItem("tasks", JSON.stringify(taskValue));
+    // const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    // tasks.push(input.value.trim());
   }
-
   // reset input value
   input.value = "";
 });
@@ -49,5 +42,19 @@ toDoList.addEventListener("click", function (e) {
 });
 
 window.addEventListener("DOMContentLoaded", function () {
-  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  // check if tasks is not null or undefined before calling forEach()
+  tasks.forEach((task) => createAndAppendTask(task));
 });
+
+// Function to create and append tasks
+function createAndAppendTask(taskText) {
+  const newTask = document.createElement("li");
+  const newCompleteBtn = document.createElement("button");
+
+  newCompleteBtn.innerText = "Completed ";
+  newTask.innerText = taskText + " ";
+
+  newTask.appendChild(newCompleteBtn);
+  toDoList.appendChild(newTask);
+}
