9cf2e55fb8444db543215691d4c0bfeffae636b9
jor-el <jerraill.d.murphy@gmail.com>
2023-03-17 07:29:05 +0100

    More JS editing `

--- JS Todos Exercise/script.js
+++ JS Todos Exercise/script.js
@@ -1,17 +1,15 @@
-// global selectors
+// GLOBAL SELECTORS
 
 const completedButton = document.querySelectorAll('li > button');
+const removeAllButton = document.querySelector('#removeAllButton');
 const taskForm = document.querySelector('#add-task');
 const taskInput = document.querySelector('input[type="text"]');
 const toDoList = document.querySelector('#todoList');
 
-// global vars
-const completed = 'Completed';
-
-// add event listener to taskInput
+// EVENT LISTENERS
 taskForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.log(taskInput.value);
+  //   console.log(taskInput.value);
 
   const taskName = taskInput.value.trim();
 
@@ -20,28 +18,78 @@ taskForm.addEventListener('submit', function (e) {
   taskInput.value = '';
 });
 
+toDoList.addEventListener('click', function (e) {
+  const target = e.target;
+  if (target === 'BUTTON');
+  {
+    if (target.parentElement.classList.contains('completed-Task')) {
+      target.parentElement.remove();
+    }
+  }
+  target.parentElement.classList.add('completed-Task');
+  target.innerText = 'Remove';
+  console.log(e);
+
+  // Update task in local storage
+  let tasks = getTasksFromLocalStorage();
+  const taskIndex = tasks.indexOf(target.parentElement.innerText);
+  if (taskIndex > -1) {
+    tasks.splice(taskIndex, 1);
+  } else {
+    tasks.push(target.parentElement.innerText);
+  }
+  saveTasksToLocalStorage(tasks);
+});
+
+removeAllButton.addEventListener('click', function (e) {
+  clearAll();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  let tasks = getTasksFromLocalStorage();
+  toDoList.innerHTML = ''; // clear existing tasks
+  tasks.forEach((task) => {
+    createTaskAndButton(task);
+  });
+});
+
+// FUNCTIONS
 function createTaskAndButton(taskValue) {
   const taskLi = document.createElement('Li');
   const taskBtn = document.createElement('Button');
+  const completed = 'Completed';
 
   taskLi.innerText = taskValue;
   taskBtn.innerText = completed;
-
   taskLi.append(taskBtn);
   toDoList.append(taskLi);
+
+  // Add task to local storage
+  let tasks = getTasksFromLocalStorage();
+  if (!tasks.includes(taskValue)) {
+    tasks.push(taskValue);
+    saveTasksToLocalStorage(tasks);
+  }
 }
 
-// completedButton is a NodeList, which is a collection of elements
-// so you need to loop through the completedButton NodeList and add the event listener to each individual button element:
-
-toDoList.addEventListener('click', function (e) {
-  //   console.log(e);
-  const button = e.target;
-  if (button === 'BUTTON');
-  {
-    if (button.parentElement.classList.contains('completed-Task')) {
-      button.parentElement.remove();
-    }
+// retrieve tasks from local storage
+function getTasksFromLocalStorage() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-  button.parentElement.classList.add('completed-Task');
-});
+  return tasks;
+}
+
+// function to save tasks to local storage
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function clearAll() {
+  localStorage.clear();
+  toDoList.innerText = '';
+  //   toDoList.innerHTML = null;
+}
