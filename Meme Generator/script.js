16c9d8a28084b1fbdd753052eeaae512950289e8
jor-el <jerraill.d.murphy@gmail.com>
2023-03-26 23:54:19 +0200

    First phase complete

--- Meme Generator/script.js
+++ Meme Generator/script.js
@@ -1,36 +1,73 @@
-// * Global Selectors //
+// https://images.unsplash.com/photo-1679421138627-76aa339a47df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80
 
-const IMAGE = document.getElementById('imageURL');
-const FORM = document.getElementById('memeGeneratorForm');
-const UPPERTEXT = document.getElementById('textOnTop');
-const LOWERTEXT = document.getElementById('textOnBottom');
+//  * Gather all the global
+const form = document.getElementById('memeGenerator');
+const imgUrl = document.getElementById('imgUrl');
+const upperText = document.getElementById('upperText');
+const lowerText = document.getElementById('lowerText');
+const imageList = document.getElementById('imageList');
 
-const ADDBTN = document.getElementById('btn-add');
-const REMOVEALLBTN = document.getElementById('btn-removeAll');
-const IMAGECONTAINER = document.getElementById('image-container');
-
-// * Retrive and append the img
-
-FORM.addEventListener('click', function (e) {
+form.addEventListener('submit', function (e) {
   e.preventDefault();
+  console.log(e.target === 'Button');
 
-  //   console.log(e.target);
+  if (!e.target === 'Button') return 0;
 
-  if (
-    e.target.tagName !== 'BUTTON' ||
-    e.target.classList.contains('btn-removeAll')
-  )
+  if (!imgUrl.value || !upperText.value || !lowerText.value) {
+    alert('Please fill in all fields!');
     return;
+  }
 
-  addImage();
+  let newMeme = createMeme();
+
+  attachTextToMeme(newMeme);
+
+  imageList.append(newMeme);
+
+  imgUrl.value = '';
+  upperText.value = '';
+  lowerText.value = '';
 });
 
-function addImage() {
-  const ImgUrl = IMAGE.value;
-  const Img = document.createElement('img');
-  Img.src = ImgUrl;
+imageList.addEventListener('click', function (e) {
+  // console.log(e.target);
 
-  IMAGECONTAINER.appendChild(Img);
+  let target = e.target;
 
-  IMAGE.value = '';
+  if (!e.target === 'IMG') return 0;
+
+  if (!confirm(`Are you sure you want to delete!`)) return 0;
+
+  // console.log(e);
+  e.target.remove();
+});
+
+function createMeme() {
+  const img = imgUrl.value;
+  const memeFrame = document.createElement('div');
+  const memeImage = document.createElement('img');
+
+  memeImage.src = img;
+  memeImage.classList.add('meme');
+  memeFrame.classList.add('memeFrame');
+
+  memeFrame.append(memeImage);
+
+  return memeFrame;
+}
+
+function attachTextToMeme(newMeme) {
+  let textOnTop = document.createElement('div');
+  let textOnBottom = document.createElement('div');
+
+  textOnTop.innerText = upperText.value;
+  textOnBottom.innerText = lowerText.value;
+
+  textOnTop.classList.add('textOnTop');
+  textOnBottom.classList.add('textOnBottom');
+
+  newMeme.append(textOnTop);
+  newMeme.append(textOnBottom);
+
+  return newMeme;
 }
